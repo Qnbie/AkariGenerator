048a891fa3b130a709e7c2b3f889360081ba35ca
Qnbie <alszenful@gmail.com>
2021-11-08 11:36:21 +0100

    Remove Unnecessary things

--- Assets/debugger.cs
+++ Assets/debugger.cs
@@ -9,9 +9,7 @@ public class debugger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var ac = new AlgorithmController();
-        var puzzle = ac.GetPuzzle(Difficulty.Easy);
-        Debug.Log(puzzle);
+        
     }
 
     // Update is called once per frame
