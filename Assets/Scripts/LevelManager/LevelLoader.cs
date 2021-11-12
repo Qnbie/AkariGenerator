40b2ce50b58dd73a57238fc75d0baee78b854217
Qnbie <alszenful@gmail.com>
2021-11-12 11:39:46 +0100

    MainMenu basic setup

---
+++ Assets/Scripts/LevelManager/LevelLoader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utils.Enums;
+
+namespace LevelManager
+{
+    public class LevelLoader : MonoBehaviour
+    {
+        public Animator transition;
+        public float transitionTime = 1f;
+
+        public static Vector2Int GameSize;
+        public static Difficulty GameDifficulty;
+
+        public void LoadNextLevel(String nextScene, Vector2Int size, Difficulty difficulty)
+        {
+            GameSize = size;
+            GameDifficulty = difficulty;
+            LoadNextLevel(nextScene);
+        }
+        
+        public void LoadNextLevel(String nextScene)
+        {
+            StartCoroutine(
+                LoadLevel(SceneManager.GetActiveScene().buildIndex + 1)
+                );
+        }
+
+        IEnumerator LoadLevel(int levelIndex)
+        {
+            // Play animation
+            
+            transition.SetTrigger("Start");
+            
+            // Wait
+
+            yield return new WaitForSeconds(transitionTime);
+
+            //Load Sceen
+            SceneManager.LoadScene(levelIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelManager/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace LevelManager
+{
+    public class MainMenu : MonoBehaviour
+    {
+
+        public void DifficultyOnClick()
+        {
+            Debug.Log("difficulty");
+        }
+
+        public void SizeOnClick()
+        {
+            Debug.Log("size");
+            
+        }
+
+        public void StartGameOnClick()
+        {
+            Debug.Log("start");
+            
+        }
+    }
+}
